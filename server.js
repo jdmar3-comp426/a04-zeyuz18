4226b4dcae12634489ca1983f8feefdae6dad1fa
zeyuz18 <zeyuz18@live.unc.edu>
2021-12-01 20:14:01 -0500

    test final

--- server.js
+++ server.js
@@ -36,21 +36,21 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+app.get("/app/user/:id",(req,res)=>{
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id=?").get(req.params.id);
 	res.status(200).json(stmt);
-});
+})
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch("/app/update/users/:id", (req, res) => {	
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?")
+app.patch("/app/update/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const result = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).send({message: result.changes + " record updated: ID " + req.params.id + " (200)"});
+	res.status(200).send({ message: result.changes + " record updated: ID " + req.params.id + " (200)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.delete("/app/update/users/:id", (req, res) => {	
-	const stmt = db.prepare("DELETE FORM userinfo WHERE id = ?")
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const result = stmt.run(req.params.id);
-	res.status(200).send({message: result.changes + " record deleted: ID " + req.params.id + " (200)"});
+	res.status(200).send({ message: result.changes + " record deleted: ID " + req.params.id + " (200)"});
 });
 // Default response for any other request
 app.use(function(req, res){
